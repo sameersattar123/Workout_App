5788bb165b348fd6d506fc7ae7600d2ea574ecbd
Sameer <sameersattar1111@gmail.com>
2024-05-04 02:05:44 +0500

    new packages

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { Link } from "react-router-dom"
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <header>
+    <div className="container">
+      <Link to="/">
+        <h1>Workout Buddy</h1>
+      </Link>
+    </div>
+  </header>
   )
 }
 
