e410784ff5a01d762f07c8d17c4cbec64f9488fd
Sameer <sameersattar1111@gmail.com>
2024-05-04 02:32:25 +0500

    model controller and routes

---
+++ server/controllers/Workout.Controller.js
@@ -0,0 +1,30 @@
+export const getWorkouts = async (req, res) => {
+  try {
+  } catch (error) {
+    console.log(error);
+  }
+};
+export const getSingleWorkout = async (req, res) => {
+  try {
+  } catch (error) {
+    console.log(error);
+  }
+};
+export const createWorkout = async (req, res) => {
+  try {
+  } catch (error) {
+    console.log(error);
+  }
+};
+export const deleteWorkout = async (req, res) => {
+  try {
+  } catch (error) {
+    console.log(error);
+  }
+};
+export const updateWorkout = async (req, res) => {
+  try {
+  } catch (error) {
+    console.log(error);
+  }
+};
--- server/models/Workout.model.js
+++ server/models/Workout.model.js
@@ -1,1 +1,25 @@
 import mongoose from "mongoose";
+
+const workoutSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    reps: {
+      type: Number,
+      required: true,
+    },
+    load: {
+      type: Number,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+export default Workout;
